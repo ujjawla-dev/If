ddd7f20073e56a9082bb71daa49004a918e1d2ac
ujjawla <ujjawla.singh@techwagger.com>
2026-03-17 19:05:37 +0530

    update

---
+++ src/app/(auth)/new-password/page.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+
+export default function CreateNewPasswordPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen px-4">
+      <div className="w-full max-w-md text-center">
+        
+    
+        <div className="mb-6 flex justify-center">
+          <Image
+            src="/logo.png"  
+            alt="Logo"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </div>
+
+        <div className="mb-10">
+          <h1 className="text-3xl font-bold mb-4">Create new password</h1>
+          <p className="text-white/80 leading-relaxed text-sm">
+            Your new password must be different from previous used passwords.
+          </p>
+        </div>
+
+        <form className="flex flex-col gap-6 text-left">
+          <div>
+            <Input 
+              label="Password" 
+              type="password" 
+              placeholder="Type your password" 
+            />
+            <p className="text-xs text-white/50 mt-2 ml-1 text-left">
+              Must be at least 8 characters
+            </p>
+          </div>
+
+          <div>
+            <Input 
+              label="Confirm Password" 
+              type="password" 
+              placeholder="Type your password" 
+            />
+            <p className="text-xs text-white/50 mt-2 ml-1 text-left">
+              Both password must match
+            </p>
+          </div>
+          
+          <Button variant="primary" type="button" className="w-full">
+            Reset Password
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
